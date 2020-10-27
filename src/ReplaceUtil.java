794067bcf01e6575f44956ce10592a412c358f1b
ysc <dlutysc@gmail.com>
2020-10-27 14:08:07 +0800

    commit

--- src/ReplaceUtil.java
+++ src/ReplaceUtil.java
@@ -38,7 +38,7 @@ public class ReplaceUtil {
 
 
     public static void main(String[] args) {
-        File src = new File("D:/yonyou/上海/脚本/test.txt");
+        File src = new File("D:/yonyou/上海/脚本/insert_epay_receive_detail_3508.sql");
         String cont = ReplaceUtil.read(src);
         System.out.println(cont);
         // 对得到的内容进行处理
@@ -58,8 +58,8 @@ public class ReplaceUtil {
         cont = cont.replaceAll("(?i)pk_name", "pay_type_name");
         cont = cont.replaceAll("(?i)cb_code", "clear_acct_bank_code");
         cont = cont.replaceAll("(?i)cb_name", "clear_acct_bank_name");
-        cont = cont.replaceAll("(?i)pay_account_no", "pay_acct_no");
-        cont = cont.replaceAll("(?i)pay_account_name", "pay_acct_name");
+        cont = cont.replaceAll("(?i)payee_account_no", "payee_acct_no");
+        cont = cont.replaceAll("(?i)payee_account_name", "payee_acct_name");
         cont = cont.replaceAll("(?i)bl_code", "source_type_code");
         cont = cont.replaceAll("(?i)bl_name", "source_type_name");
         cont = cont.replaceAll("(?i)bsi_code", "dep_bgt_eco_code");
